032744e36199ef2ab26333775343606d873bb494
Rakesh Kumar <rakeshkr479@gmail.com>
2024-12-04 00:22:49 +0530

    index mofication

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer class="mt-4" style="background-image: url(assets/img/tummy/footer.jpg);background-color: #f5f8fd;padding-top:0;">
+   <div class="container">
+      <div class="row">
+         
+       
+        
+      </div>
+      <div class="footer-bootem" style="margin-top: 0;">
+         <h6><span>© 2024 Hello Tummy</span> | All Rights Reserved.</h6>
+         <div class="header-social-media">
+            <a href="https://thaliwalaa.com/">Thaliwalaa</a>
+            <a href="https://burnerstory.com/">Burner Story</a>
+            <a href="#">Homely Aroma</a>
+          
+         </div>
+      </div>
+   </div>
+</footer>
+<!-- progress -->
+<div id="progress">
+      <span id="progress-value"><i class="fa-solid fa-arrow-up"></i></span>
+</div>
+
+<!-- Bootstrap Js -->
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<!-- fancybox -->
+<script src="assets/js/jquery.fancybox.min.js"></script>
+<script src="assets/js/custom.js"></script>
+
+<!-- Form Script -->
+<script src="assets/js/contact.js"></script>
+<script type="text/javascript" src="assets/js/sweetalert.min.js"></script>
+</body>
---
+++ header.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello Tummy - Cloud kitchen</title>
+  <link rel="icon" href="assets/img/tummy/favicon-hello.png">
+  <!-- CSS only -->
+   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+   <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+   <!-- fancybox -->
+   <link rel="stylesheet" href="assets/css/jquery.fancybox.min.css">
+   <!-- Font Awesome 6 -->
+   <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+   <!-- style -->
+   <link rel="stylesheet" href="assets/css/style.css">
+   <!-- responsive -->
+   <link rel="stylesheet" href="assets/css/responsive.css">
+   <!-- color -->
+   <link rel="stylesheet" href="assets/css/color.css">
+   <!-- jQuery -->
+   <script src="assets/js/jquery-3.6.0.min.js"></script>
+   <script src="assets/js/preloader.js"></script>
+ </head>
+<body>
+<!-- preloader -->
+<div class="preloader"> 
+    <div class="container">
+      <div class="dot dot-1"></div>
+      <div class="dot dot-2"></div>
+      <div class="dot dot-3"></div>
+    </div>
+</div>
+<!-- end preloader -->
+<header class="one">
+  <div class="top-header">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-xl-6">
+               <div class="d-flex align-items-center">
+                  <div class="content-header me-5">
+                    <i>
+                        <svg height="512" viewBox="0 0 32 32" width="512" xmlns="http://www.w3.org/2000/svg"><g id="_16-Smartphone" data-name="16-Smartphone"><path d="m23 2h-14a3 3 0 0 0 -3 3v22a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-22a3 3 0 0 0 -3-3zm-5.39 2-.33 1h-2.56l-.33-1zm6.39 23a1 1 0 0 1 -1 1h-14a1 1 0 0 1 -1-1v-22a1 1 0 0 1 1-1h3.28l.54 1.63a2 2 0 0 0 1.9 1.37h2.56a2 2 0 0 0 1.9-1.37l.54-1.63h3.28a1 1 0 0 1 1 1z"/><path d="m17 24h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2z"/></g></svg>
+                    </i><h4>Phone:<a href="callto:+919916104429" style="color:#000;">9916104429</a></h4>
+                  </div>
+<!--
+                  <div class="content-header">
+                    <i>
+                        <svg height="512" viewBox="0 0 32 32" width="512" xmlns="http://www.w3.org/2000/svg"><g id="_01-Email" data-name="01-Email"><path d="m29.61 12.21-13-10a1 1 0 0 0 -1.22 0l-13 10a1 1 0 0 0 -.39.79v14a3 3 0 0 0 3 3h22a3 3 0 0 0 3-3v-14a1 1 0 0 0 -.39-.79zm-13.61-7.95 11.36 8.74-11.36 8.74-11.36-8.74zm11 23.74h-22a1 1 0 0 1 -1-1v-12l11.39 8.76a1 1 0 0 0 1.22 0l11.39-8.76v12a1 1 0 0 1 -1 1z"/></g></svg>
+                     </i><h4>Email:<a href="mailto:+1(850)344066">info@hellotummy.com</a></h4>
+                  </div>
+-->
+               </div>
+            </div>
+            <div class="col-xl-6">
+               <div class="text-end">
+               <div class="header-social-media">
+                  <a href="#">
+                     Facebook
+                  </a>
+                  <a href="#">
+                     Instagram
+                  </a>
+                  <a href="#">
+                     Youtube
+                  </a>   
+               </div>
+               
+               </div>
+            </div>
+        </div>
+    </div>
+  </div>
+  <div class="bottom-bar ">
+    <div class="container">
+         <div class="row align-items-center">
+            <div class="col-xl-3">
+               <div class="d-flex align-items-center justify-content-between">
+                  <div class="logo">
+                     <a href="index.php">
+<!--                         <h3 style="color:#FFD40D;">Hello <span style="color:#F3274C;">Tummy</span></h3>-->
+                        <img alt="logo" src="assets/img/tummy/hellotummy-removebg-preview.png" style="width:180px;">
+                     </a>
+                  </div>
+                  <div class="d-flex cart-checkout">
+<!--
+                    <a href="cart-checkout.html">
+                        <i>
+                            <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><g>
+                            <path d="m452 120h-60.946c-7.945-67.478-65.477-120-135.054-120s-127.109 52.522-135.054 120h-60.946c-11.046 0-20 8.954-20 20v352c0 11.046 8.954 20 20 20h392c11.046 0 20-8.954 20-20v-352c0-11.046-8.954-20-20-20zm-196-80c47.484 0 87.019 34.655 94.659 80h-189.318c7.64-45.345 47.175-80 94.659-80zm176 432h-352v-312h40v60c0 11.046 8.954 20 20 20s20-8.954 20-20v-60h192v60c0 11.046 8.954 20 20 20s20-8.954 20-20v-60h40z"></path></g></svg>
+                        </i>
+                    </a>
+-->
+                    <div class="bar-menu">
+                         <i class="fa-solid fa-bars"></i>
+                    </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-xl-6">
+               <nav class="navbar">
+                  <ul class="navbar-links">
+                    
+                    
+                    <li class="navbar-dropdown">
+                      <a href="index.php">Home</a>
+                    </li>
+                    <li class="navbar-dropdown">
+                      <a href="#about">About Us</a>
+                    </li>
+                    <li class="navbar-dropdown">
+                      <a href="#why">Why Choose Us?</a>
+                    </li>
+                   
+                  </ul>
+                </nav>
+            </div>
+            <div class="col-lg-3">
+               <div class="hamburger-icon">
+                 
+                     <a href="#contact" class="button">Contact</a>
+               </div>
+            </div>
+         </div>
+   </div>  
+  </div>
+  <div class="mobile-nav hmburger-menu" id="mobile-nav" style="display:block;">
+      <div class="res-log">
+        <a href="index.php">
+<!--            <h3>Hello Tummy</h3>-->
+          <img src="assets/img/tummy/hellotummy-removebg-preview.png" alt="Responsive Logo" class="white-logo" style="width:150px;">
+        </a>
+      </div>
+        <ul>
+
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#why">Why Choose Us?</a></li>
+         
+          <li><a href="#contact">Contact us</a></li>
+
+          </ul>
+
+          <a href="JavaScript:void(0)" id="res-cross" style="color:red;position: absolute;
+    right: 5%;
+    top: 1%;">x</a>
+  </div>
+</header>
